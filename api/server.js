8a873d4cdb94ac826cd504dd36dbf81b7d2bac9c
Kavin Tjhan <tjhan.kavin@gmail.com>
2020-11-07 12:23:12 -0800

    second commit

--- api/server.js
+++ api/server.js
@@ -2,11 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-// const authenticate = require('../auth/restricted-middleware');
-// const authRouter = require('../auth/auth-router');
-// const usersRouter = require('../users/users-router');
-// const inputsRouter = require('../inputs/inputs-router');
-
 const server = express();
 
 server.use(helmet());
@@ -16,9 +11,5 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.send("Welcome to Doctor's office");
 });
-
-// server.use('/api/auth', authRouter);
-// server.use('/api/users', authenticate, usersRouter);
-// server.use('/api/inputs', authenticate, inputsRouter);
 
 module.exports = server;
